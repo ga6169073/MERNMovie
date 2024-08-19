9261d0292796fd20e29c245aa7fc70d80cd8e84a
ga6169073 <74145858+ga6169073@users.noreply.github.com>
2024-08-20 02:13:15 +0700

    fixxx

--- client/src/Redux/Protection.js
+++ client/src/Redux/Protection.js
@@ -1,11 +1,12 @@
 import { logoutAction } from "./Actions/userActions.js"
-
+import { useNavigate } from "react-router-dom"
 export const ErrorActions = (error, dispatch, action) => {
     const message = error.response && error.response.data.message ? error.response.data.message : error.message
+    const navigate = useNavigate()
     if (message === "Not authorized, token failed") {
         // logout here
         dispatch(logoutAction())
-
+        navigate("/login")
     }
     return dispatch({ type: action, payload: message })
 }
